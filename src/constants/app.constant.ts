5630a0ebe870e5d6f5b4b4dc8f92a7e0fb60dd81
ranggakrisnaa <infinity.duos@gmail.com>
2025-03-22 22:49:03 +0700

    fix: change image default path url

--- src/constants/app.constant.ts
+++ src/constants/app.constant.ts
@@ -33,7 +33,7 @@ export const loggingRedactPaths = [
 export const DEFAULT = {
   IMAGE_PATH: process.env.APP_IMAGE_PATH,
   IMAGE_DEFAULT:
-    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Ddefault&psig=AOvVaw1Sy_FwWaBtHLLskq7vEEZz&ust=1740628833402000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJi4iu254IsDFQAAAAAdAAAAABAE',
+    'https://res.cloudinary.com/dbuyqvhts/image/upload/v1742657109/uploads/hl8ttu0ry2rosdr9sjb4.png',
 };
 
 export const INITIAL_VALUE = {
